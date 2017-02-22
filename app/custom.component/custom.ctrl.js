a664130bc38ce0ebafba3559435ac267a31eb7a7
nviser <super_155305@ukr.net>
2017-02-22 03:55:20 +0200

    image validation added

--- app/custom.component/custom.ctrl.js
+++ app/custom.component/custom.ctrl.js
@@ -6,6 +6,7 @@ angular.module('store').controller('customCtrl', ['$scope',
         $scope.part = '../../assets/img/';
         $scope.addProd = function (arg) {
             $scope.warn = false;
+            $scope.notification = false;
             if ($scope.logo && $scope.id && $scope.name && $scope.description && $scope.price) {
                 angular.forEach(products, function (value, key) {
                     if (value.id == $scope.id && $scope.key != key) {
@@ -13,27 +14,35 @@ angular.module('store').controller('customCtrl', ['$scope',
                     }
                 });
                 if (!$scope.warn) {
-                $scope.notification = false;
-                    var newProd = {
-                        id: $scope.id,
-                        name: $scope.name,
-                        description: $scope.description,
-                        price: $scope.price,
-                        img: $scope.logo
-                    };
-                    $scope.products = JSON.parse(localStorage.getItem('products'));
-                    $scope.products.push(newProd);
-                    var str = JSON.stringify($scope.products);
-                    localStorage.setItem('products', str);
-                    $scope.saved = true;
+                    if (!$scope.fails) {
+                        var newProd = {
+                            id: $scope.id,
+                            name: $scope.name,
+                            description: $scope.description,
+                            price: $scope.price,
+                            img: $scope.logo
+                        };
+                        $scope.products = JSON.parse(localStorage.getItem('products'));
+                        $scope.products.push(newProd);
+                        var str = JSON.stringify($scope.products);
+                        localStorage.setItem('products', str);
+                        $scope.saved = true;
+                    } 
                 }
             } else {
                 $scope.notification = true;
             }
         }
         $scope.saveLogo = function (e) {
+            var _URL = window.URL || window.webkitURL;
             $scope.logo = e.files[0].name;
+            $scope.fails = false;
             $scope.path = $scope.part + $scope.logo;
+            var img = new Image();
+            img.onload = function () {
+                if (this.width > 400 || this.height > 400) $scope.fails = true;
+            }
+            img.src = _URL.createObjectURL(e.files[0]);
             $scope.$apply();
         }
         $scope.path = $scope.part + $scope.logo;
--- app/prod.component/productCtrl.js
+++ app/prod.component/productCtrl.js
@@ -18,11 +18,19 @@ angular.module('store').controller('productCtrl', ['$scope', '$location', '$rout
         $scope.saveLogo = function (e) {
             $scope.logo = e.files[0].name;
             $scope.path = $scope.part + $scope.logo;
+            var _URL = window.URL || window.webkitURL;
+            $scope.fails = false;
+            var img = new Image();
+            img.onload = function () {
+                if (this.width > 400 || this.height > 400) $scope.fails = true;
+            }
+            img.src = _URL.createObjectURL(e.files[0]);
             $scope.$apply();
         }
         $scope.path = $scope.part + $scope.logo;
         $scope.addProd = function (arg) {
             $scope.warn = false;
+            $scope.notification = false;
             if ($scope.product.id && $scope.product.name && $scope.product.description && $scope.product.price) {
                 angular.forEach(products, function (value, key) {
                     if (value.id == $scope.product.id && $scope.key != key) {
@@ -30,19 +38,20 @@ angular.module('store').controller('productCtrl', ['$scope', '$location', '$rout
                     }
                 });
                 if (!$scope.warn) {
-                    $scope.notification = false;
-                    var newProd = {
-                        id: $scope.product.id,
-                        name: $scope.product.name,
-                        description: $scope.product.description,
-                        price: $scope.product.price,
-                        img: $scope.logo
-                    };
-                    products.splice($scope.key, 1, newProd);
-                    var str = JSON.stringify(products);
-                    localStorage.setItem('products', str);
-                    $scope.editProduct = false;
-                    $scope.saved = true;
+                    if (!$scope.fails) {
+                        var newProd = {
+                            id: $scope.product.id,
+                            name: $scope.product.name,
+                            description: $scope.product.description,
+                            price: $scope.product.price,
+                            img: $scope.logo
+                        };
+                        products.splice($scope.key, 1, newProd);
+                        var str = JSON.stringify(products);
+                        localStorage.setItem('products', str);
+                        $scope.editProduct = false;
+                        $scope.saved = true;
+                    }
                 }
             } else {
                 $scope.notification = true;

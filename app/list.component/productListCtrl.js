7990c8b03c93e30d974f7240316db81ae33534e5
Vitaliy Nezhenets <1111vitt77@gmail.com>
2017-02-22 13:40:05 +0200

    remove console.log(e)

--- app/list.component/productListCtrl.js
+++ app/list.component/productListCtrl.js
@@ -6,7 +6,6 @@ angular.module('store').controller('productListCtrl', ['_products', '$route', '$
             $location.path('/product/' + item.id);
         }
         $scope.delete = function (product) {
-            console.log(e);
             var flag = confirm('Are you shure want to remove this product?');
             if(flag){
                 var key = $scope.products.indexOf(product);

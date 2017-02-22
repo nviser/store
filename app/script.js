0b152ea5e6c47747789f3dea908ca014ac976d6c
nviser <super_155305@ukr.net>
2017-02-22 04:16:28 +0200

    minified

---
+++ app/script.js
@@ -0,0 +1,7 @@
+// @koala-append "store.module.js"
+// @koala-append "service/service.js"
+// @koala-append "init/config.js"
+// @koala-append "list.component/productListCtrl.js"
+
+// @koala-append "prod.component/productCtrl.js"
+// @koala-append "custom.component/custom.ctrl.js"
